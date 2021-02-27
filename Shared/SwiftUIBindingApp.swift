980c3b94e4325f6e2c2e75bc767323baae6f5c00
Alex Nagy <rebeloper@users.noreply.github.com>
2021-02-27 19:03:13 +0200

    Initial Commit

---
+++ Shared/SwiftUIBindingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBindingApp.swift
+//  Shared
+//
+//  Created by Alex Nagy on 27.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBindingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
